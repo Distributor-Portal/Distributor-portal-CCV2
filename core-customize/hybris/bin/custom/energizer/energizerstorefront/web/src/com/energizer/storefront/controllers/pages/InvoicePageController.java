c41c849d941c080555b95c141bb518b7d30ec520
Munna Singh (Americas 1 - iDEAS-DX) <mu20325022@wipro.com>
2022-05-13 00:28:36 +0530

    changes for invoice PDF

--- core-customize/hybris/bin/custom/energizer/energizerstorefront/web/src/com/energizer/storefront/controllers/pages/InvoicePageController.java
+++ core-customize/hybris/bin/custom/energizer/energizerstorefront/web/src/com/energizer/storefront/controllers/pages/InvoicePageController.java
@@ -13,16 +13,21 @@
  */
 package com.energizer.storefront.controllers.pages;
 
+import de.hybris.platform.b2bacceleratorfacades.order.B2BOrderFacade;
 import de.hybris.platform.cms2.exceptions.CMSItemNotFoundException;
+import de.hybris.platform.commercefacades.order.data.OrderData;
+import de.hybris.platform.util.Config;
 
 import java.io.IOException;
 import java.io.OutputStream;
 import java.io.PrintWriter;
+import java.net.URISyntaxException;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.lang.StringUtils;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -30,8 +35,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.energizer.core.azure.blob.EnergizerWindowsAzureBlobStorageStrategy;
 import com.energizer.facades.order.impl.DefaultEnergizerInvoiceFacade;
 import com.energizer.storefront.annotations.RequireHardLogIn;
+import com.microsoft.azure.storage.StorageException;
+import com.microsoft.azure.storage.blob.CloudBlobContainer;
+import com.microsoft.azure.storage.blob.CloudBlobDirectory;
+import com.microsoft.azure.storage.blob.CloudBlockBlob;
+import com.microsoft.azure.storage.blob.ListBlobItem;
 
 
 /**
@@ -52,18 +63,26 @@ public class InvoicePageController extends AbstractSearchPageController
 	@Resource(name = "defaultInvoiceFacade")
 	private DefaultEnergizerInvoiceFacade defaultInvoiceFacade;
 
+	@Resource
+	private EnergizerWindowsAzureBlobStorageStrategy energizerWindowsAzureBlobStorageStrategy;
+
+	@Resource(name = "b2bOrderFacade")
+	private B2BOrderFacade orderFacade;
 
 	@RequestMapping(value = "/invoicePdfDisplay", method = RequestMethod.GET)
 	@RequireHardLogIn
-	public void invoice(@RequestParam("orderCode") final String invoiceNumber,
-			@RequestParam(value = "inline", required = false) final Boolean inline, final Model model,
-			final HttpServletRequest request, final HttpServletResponse response) throws CMSItemNotFoundException, IOException
+	public void invoice(@RequestParam("orderCode")
+	final String invoiceNumber, @RequestParam(value = "inline", required = false)
+	final Boolean inline, final Model model, final HttpServletRequest request, final HttpServletResponse response)
+			throws CMSItemNotFoundException, IOException
 	{
 
-		final byte pdfFile[] = defaultInvoiceFacade.getPDFInvoiceAsBytes(this.getCmsSiteService().getCurrentSite().getUid(),
-				invoiceNumber.trim());
+		//final byte pdfFile[] = defaultInvoiceFacade.getPDFInvoiceAsBytes(this.getCmsSiteService().getCurrentSite().getUid(),
+		//invoiceNumber.trim());
 
 		//final byte pdfFile[] = defaultEnergizerInvoiceService.getPDFInvoiceAsBytes(invoiceNumber);
+		final OrderData orderData = orderFacade.getOrderDetailsForCode(invoiceNumber.trim());
+		final byte pdfFile[] = getInvoiceFileFromBlob(orderData.getErpOrderNumber());
 
 		if (null != pdfFile)
 		{
@@ -99,4 +118,61 @@ public class InvoicePageController extends AbstractSearchPageController
 
 
 	}
+
+	public byte[] getInvoiceFileFromBlob(final String erpOrderNo)
+	{
+		byte[] invoiceFile = null;
+		System.out.println("erpOrderNo--->" + erpOrderNo);
+		if (StringUtils.isNotEmpty(erpOrderNo))
+		{
+			CloudBlobDirectory blobDirectory = null;
+			final CloudBlobContainer container = energizerWindowsAzureBlobStorageStrategy.getBlobContainer();
+			final String filePath = Config.getParameter("invoice.filepath.EMEA");
+			System.out.println("filePath--->" + filePath);
+			try
+			{
+				blobDirectory = container.getDirectoryReference(filePath);
+
+				for (final ListBlobItem blobItem : blobDirectory.listBlobs())
+				{
+					System.out.println("Method--> getInvoiceFileFromBlob--->Start");
+					final String subfullFilePath = blobItem.getStorageUri().getPrimaryUri().getPath();
+					System.out.println("subfullFilePath-->" + subfullFilePath);
+					final String fullFilePath = subfullFilePath.substring(8);
+					System.out.println("fullFilePath-->" + fullFilePath);
+					final String fileName = StringUtils.substringAfterLast(fullFilePath, "/");
+					System.out.println("fileName-->" + fileName);
+					if (fileName.contains(erpOrderNo))
+					{
+						System.out.println("erpOrderNo-->" + erpOrderNo);
+						final CloudBlockBlob blob2 = container.getBlockBlobReference(filePath.toString());
+						invoiceFile = blob2.downloadText().getBytes();
+						break;
+
+					}
+					System.out.println("Method--> getInvoiceFileFromBlob--->End");
+
+				}
+			}
+			catch (StorageException | URISyntaxException e)
+			{
+				// YTODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			catch (final IOException e)
+			{
+				// YTODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+		}
+
+		return invoiceFile;
+	}
+
+
+
+
+
+
 }
