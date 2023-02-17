b6a2e597e7ffabdd43ecf7d11ae9fe2e715c383f
sudeepthedgewell <venkata.manne@edgewell.com>
2023-02-17 17:04:22 +0530

    test facets with multiple shipping points

--- core-customize/hybris/bin/custom/energizer/energizercore/src/com/energizer/core/solr/provider/ShippingPointFacetDisplayNameProvider.java
+++ core-customize/hybris/bin/custom/energizer/energizercore/src/com/energizer/core/solr/provider/ShippingPointFacetDisplayNameProvider.java
@@ -1,5 +1,6 @@
 package com.energizer.core.solr.provider;
 
+import com.energizer.services.product.EnergizerProductService;
 import de.hybris.platform.solrfacetsearch.config.IndexedProperty;
 import de.hybris.platform.solrfacetsearch.provider.impl.AbstractFacetValueDisplayNameProvider;
 import de.hybris.platform.solrfacetsearch.search.SearchQuery;
@@ -7,11 +8,27 @@ import org.apache.commons.lang.StringUtils;
 
 public class ShippingPointFacetDisplayNameProvider extends AbstractFacetValueDisplayNameProvider {
 
+    private EnergizerProductService energizerProductService;
+
+    public EnergizerProductService getEnergizerProductService() {
+        return energizerProductService;
+    }
+
+    public void setEnergizerProductService(EnergizerProductService energizerProductService) {
+        this.energizerProductService = energizerProductService;
+    }
+
     @Override
     public String getDisplayName(final SearchQuery query, final IndexedProperty property, final String facetValue)
     {
         if(facetValue.contains("AccountNo")){
-            return StringUtils.substringBefore(facetValue,"AccountNo");
+            String shippingPoint = StringUtils.substringBefore(facetValue,"AccountNo").trim();
+            String shippingPointName = energizerProductService.getShippingPointName(shippingPoint);
+            if(null != shippingPointName ){
+                return shippingPointName;
+            }else {
+               return  shippingPoint;
+            }
         }else {
             return facetValue;
         }
