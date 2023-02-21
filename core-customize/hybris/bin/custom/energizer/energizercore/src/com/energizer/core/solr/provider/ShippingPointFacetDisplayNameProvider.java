7d92db1fa50e0dccbe090754abcdf9ef413035fa
sudeepthedgewell <venkata.manne@edgewell.com>
2023-02-21 12:28:22 +0530

    Shipping Point Fix

---
+++ core-customize/hybris/bin/custom/energizer/energizercore/src/com/energizer/core/solr/provider/ShippingPointFacetDisplayNameProvider.java
@@ -0,0 +1,36 @@
+package com.energizer.core.solr.provider;
+
+import com.energizer.services.product.EnergizerProductService;
+import de.hybris.platform.solrfacetsearch.config.IndexedProperty;
+import de.hybris.platform.solrfacetsearch.provider.impl.AbstractFacetValueDisplayNameProvider;
+import de.hybris.platform.solrfacetsearch.search.SearchQuery;
+import org.apache.commons.lang.StringUtils;
+
+public class ShippingPointFacetDisplayNameProvider extends AbstractFacetValueDisplayNameProvider {
+
+    private EnergizerProductService energizerProductService;
+
+    public EnergizerProductService getEnergizerProductService() {
+        return energizerProductService;
+    }
+
+    public void setEnergizerProductService(EnergizerProductService energizerProductService) {
+        this.energizerProductService = energizerProductService;
+    }
+
+    @Override
+    public String getDisplayName(final SearchQuery query, final IndexedProperty property, final String facetValue)
+    {
+        if(facetValue.contains("_")){
+            String shippingPoint = StringUtils.substringBefore(facetValue,"_").trim();
+            String shippingPointName = energizerProductService.getShippingPointName(shippingPoint);
+            if(null != shippingPointName ){
+                return shippingPointName;
+            }else {
+                return  shippingPoint;
+            }
+        }else {
+            return facetValue;
+        }
+    }
+}
